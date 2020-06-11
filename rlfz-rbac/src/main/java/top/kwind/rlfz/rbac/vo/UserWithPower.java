7bfca8d4f92225a4f5e46fef139cd6e8f8696217
2422494482 <xagu_qc@foxmail.com>
2020-06-11 12:02:45 +0800

    设置默认主题

--- rlfz-rbac/src/main/java/top/kwind/rlfz/rbac/vo/UserWithPower.java
+++ rlfz-rbac/src/main/java/top/kwind/rlfz/rbac/vo/UserWithPower.java
@@ -1,6 +1,7 @@
 package top.kwind.rlfz.rbac.vo;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import top.kwind.rlfz.rbac.pojo.Power;
 import top.kwind.rlfz.rbac.pojo.User;
 
@@ -12,8 +13,11 @@ import java.util.List;
  * Email:xagu_qc@foxmail.com
  * Describe: TODO
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class UserWithPower extends User {
 
     List<Power> powers;
+
+    private static final long serialVersionUID = 1L;
 }
