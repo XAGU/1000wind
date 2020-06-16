34c0955bc743a4efbab9b2f86b954e571214b3c4
XAGU <xagu_qc@foxmail.com>
2020-06-16 21:09:33 +0800

    签到功能完善 (#29)
    签到功能完善

--- rlfz-sign/src/main/java/top/kwind/rlfz/sign/mapper/UserSignMapper.java
+++ rlfz-sign/src/main/java/top/kwind/rlfz/sign/mapper/UserSignMapper.java
@@ -1,6 +1,7 @@
 package top.kwind.rlfz.sign.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import top.kwind.rlfz.sign.pojo.UserCourseSign;
 import top.kwind.rlfz.sign.pojo.UserSign;
 
 import java.util.List;
@@ -11,7 +12,7 @@ public interface UserSignMapper {
     /**
      * 通过用户ID查询该用户的所有签到信息
      */
-    List<UserSign> selectByUser(String userId);
+    List<UserCourseSign> selectByUser(String courseId,Integer userId);
 
 
     /**
@@ -20,4 +21,11 @@ public interface UserSignMapper {
      * @return
      */
     int insertUserSign(UserSign userSign);
+
+    /**
+     * 通过id批量删除
+      * @param ids
+     * @return
+     */
+    int batchDelete(String[] ids);
 }
